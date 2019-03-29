d4bbe516b057b86e17adb11ab3970ada1d1a0b59
Michael Larragueta <m.larragueta@yahoo.com>
2019-03-29 10:27:04 -0700

    Add scripts for saving answers

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,20 @@
 $(function(){
-  var answer1 = $("input:radio[name=q1]:checked").val();
-  var answer2 = $("input:radio[name=q2]:checked").val();
-  var answer3 = $("input:radio[name=q3]:checked").val();
-  var answer4 = $("input:radio[name=q4]:checked").val();
-  var answer5 = $("input:radio[name=q5]:checked").val();
+
+  $("form#qq").submit(function(event){
+    var answer1 = $("input:radio[name=q1]:checked").val();
+    var answer2 = $("input:radio[name=q2]:checked").val();
+    var answer3 = $("input:radio[name=q3]:checked").val();
+    var answer4 = $("input:radio[name=q4]:checked").val();
+    var answer5 = $("input:radio[name=q5]:checked").val();
+    console.log(answer1);
+    console.log(answer2);
+    console.log(answer3);
+    console.log(answer4);
+    console.log(answer5);
+      
+    event.preventDefault();
+
+  });
 
 
 
