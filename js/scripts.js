bcbdb7cd099e38d5cc3864f2d6985e8f8c6ea29c
Michael Larragueta <m.larragueta@yahoo.com>
2019-03-29 11:26:40 -0700

    add changes to format. Fix menu option. And provide for specific result

--- js/scripts.js
+++ js/scripts.js
@@ -7,11 +7,6 @@ $(function(){
     var answer4 = parseInt($("input:radio[name=q4]:checked").val());
     var answer5 = parseInt($("input:radio[name=q5]:checked").val());
     var total = answer1 + answer2 + answer3 + answer4 + answer5;
-    console.log(answer1);
-    console.log(answer2);
-    console.log(answer3);
-    console.log(answer4);
-    console.log(answer5);
         if (total === 40 || total === 39 || total === 37 || total === 47 || total === 41 || total === 42 || total === 51 || total === 50 || total === 52){
             if (answer1 === 1 && answer2 === 3 && answer3 === 9 && answer4 === 11 && answer5 === 16){
               //total = 40
