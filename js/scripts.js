b772c12c14d5fd908d02f6c6a2f9475af9495746
Michael Larragueta <m.larragueta@yahoo.com>
2019-03-29 10:14:00 -0700

    add submit button and selection values

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(function(){
+  var answer1 = $("input:radio[name=q1]:checked").val();
+  var answer2 = $("input:radio[name=q2]:checked").val();
+  var answer3 = $("input:radio[name=q3]:checked").val();
+  var answer4 = $("input:radio[name=q4]:checked").val();
+  var answer5 = $("input:radio[name=q5]:checked").val();
+
+
+
+
+
+});
